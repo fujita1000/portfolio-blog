3d7f1a7b1cb938c9cb9553fb7636ecf7ebf208c5
{fujitanao} <fujitanao.rmt@gmail.com>
2023-01-11 20:17:33 +0900

    Initial Commit

--- src/components/Top_bg.tsx
+++ src/components/Top_bg.tsx
@@ -10,7 +10,13 @@ const Top_bg = () =>
             よく使う空爆やセット、壁抜きの記事を載せてるよ
           </h2>
           <div className='h-[120px] w-screen md:h-[200px] xl:h-[200px] 2xl:h-[250px]'>
-            <Image src={'/dev/header.jpg'} layout='fill' alt='背景画像' priority={true} ></Image>
+            <Image
+              src={'/dev/header.jpg'}
+              layout='fill'
+              objectFit='cover'
+              alt='背景画像'
+              priority={true}
+            ></Image>
           </div>
         </div>
       </>
