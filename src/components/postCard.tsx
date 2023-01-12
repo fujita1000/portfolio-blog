def279fbdddbf0fea9dfe8ef08ce92288eaef6fd
{fujitanao} <fujitanao.rmt@gmail.com>
2023-01-13 00:33:34 +0900

    Initial Commit

--- src/components/postCard.tsx
+++ src/components/postCard.tsx
@@ -5,7 +5,6 @@ import { post } from '../utils/types';
 const PostCard = ({ post }: post) => {
   return (
     <Link href={`/posts/${post.slug}`}>
-      <a>
         <div className='my-4 h-[250px] w-[350px] lg:w-full xl:w-[350px] 2xl:mb-[150px] 2xl:w-[400px]'>
           <div className='relative mb-4 h-[197px] w-[350px] lg:w-full xl:w-[350px] 2xl:h-[225px] 2xl:w-[400px]'>
             <Image src={`/${post.frontMatter.image}`} alt={post.frontMatter.title} layout='fill' />
@@ -31,7 +30,6 @@ const PostCard = ({ post }: post) => {
             <p className='text-1xl lg:text-[18px] 2xl:text-[20px]'>{post.frontMatter.date}</p>
           </div>
         </div>
-      </a>
     </Link>
   );
 };
